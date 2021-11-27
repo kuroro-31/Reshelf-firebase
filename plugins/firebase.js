2572ee7e6cef3b15527077abd883e1a7d783a774
Shun-ichiroTanaka <shun.ichiro.tanaka16@gmail.com>
2021-11-28 03:04:43 +0900

    修正

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -3,14 +3,6 @@ import { getAnalytics } from 'firebase/analytics'
 import { initializeApp } from 'firebase/app'
 
 const firebaseConfig = {
-  // apiKey: process.env.FIREBASE_API_KEY,
-  // authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  // databaseURL: process.env.FIREBASE_DATABASE_URL,
-  // projectId: process.env.FIREBASE_PROJECT_ID,
-  // storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  // messagingSenderId: process.env.FIREBASE_MESSSAGE_SENDER_ID,
-  // appId: process.env.FIREBASE_APP_ID,
-  // measurementId: process.env.FIREBASE_MEASUREMENT_ID,
   apiKey: 'AIzaSyB52K0YyvLSsM1C1G0PU-ueoSskdH5yfMI',
   authDomain: 'reshelf-nuxt-app.firebaseapp.com',
   databaseURL:
