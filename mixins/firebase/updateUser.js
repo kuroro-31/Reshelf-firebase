1befe6930efa6c9e394538888736a8e946cab206
Shun-ichiroTanaka <shun.ichiro.tanaka16@gmail.com>
2021-12-01 12:29:45 +0900

    自動保存に変更

--- mixins/firebase/updateUser.js
+++ mixins/firebase/updateUser.js
@@ -1,19 +1,32 @@
 import { getAuth, updateProfile } from 'firebase/auth'
+import _ from 'lodash'
 
 export const updateUser = {
+  data() {
+    return {
+      alert: '',
+    }
+  },
+  watch: {
+    post: {
+      handler: _.debounce(function () {
+        this.updateProfile()
+      }, 2000), // memosのデータの更新が終わった2秒後に実行される
+      deep: true,
+    },
+  },
   methods: {
     updateProfile() {
+      this.alert = '保存中です...'
       const auth = getAuth()
       updateProfile(auth.currentUser, {
         displayName: this.fb_user.name,
         photoURL: this.fb_user.img,
       })
         .then(() => {
-          // Profile updated!
-          console.log('Profile updated!')
+          this.alert = 'アップデートしました'
         })
         .catch((error) => {
-          // An error occurred
           console.log(error)
         })
     },
--- pages/user/profile.vue
+++ pages/user/profile.vue
@@ -61,27 +61,8 @@
                         />
                       </div>
                     </div>
-                    <div class="flex items-center justify-center w-full">
-                      <button
-                        type="submit"
-                        class="
-                          mt-9
-                          font-semibold
-                          leading-none
-                          text-white
-                          py-4
-                          px-10
-                          bg-blue-700
-                          rounded
-                          hover:bg-blue-600
-                          focus:ring-2
-                          focus:ring-offset-2
-                          focus:ring-blue-700
-                          focus:outline-none
-                        "
-                      >
-                        更新する
-                      </button>
+                    <div class="">
+                      {{ alert }}
                     </div>
                   </form>
                 </div>
