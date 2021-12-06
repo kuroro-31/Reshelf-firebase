8bf89875e761d2dad42a12a1abdc3bd40ed392a3
Shun-ichiroTanaka <shun.ichiro.tanaka16@gmail.com>
2021-12-06 13:17:09 +0900

    hosting basic認証

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,16 @@
 const functions = require('firebase-functions')
+const express = require('express')
+const basicAuth = require('basic-auth-connect')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express()
+
+app.all(
+  '/*',
+  basicAuth(function (user, password) {
+    return user === 'reshelf' && password === 'devpass'
+  })
+)
+
+app.use(express.static(__dirname + '/static/'))
+
+exports.app = functions.https.onRequest(app)
